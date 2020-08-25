2e5e57eeb067036f51c6324889c522d18352a47b
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-25 23:39:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,7 +41,8 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  winter_supplies = holiday_hash[:winter].select {|holiday, supplies|
+  winter_supplies = holiday_hash[:winter].select do |holiday, supplies|
+    
     
 end
 
