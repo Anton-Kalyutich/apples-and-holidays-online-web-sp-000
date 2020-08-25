6060262fb169663bb5d01cb26f6ecaa9fb27ba3a
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-25 23:48:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,6 +55,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  string = ""
   holiday_hash.each do |season, hash|
     hash.each do |holiday, supplies|
       
