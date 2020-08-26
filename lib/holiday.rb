b130c9d5301f252ff431516e5202f59ec1efc15b
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-26 00:13:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,8 +66,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  bbq_holidays =[]
   holiday_hash.each_value do |hash|
-    bbq_holidays =[]
     hash.each do |holiday, supplies|
       bbq_holidays << holiday if supplies.include?("BBQ")
     end
