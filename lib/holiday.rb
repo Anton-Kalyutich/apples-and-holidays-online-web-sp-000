a1d5765488e8a9c54629f93ebcf693a8160514e1
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-08-26 00:07:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.to_s.capitalize}:"
     string = ""
     hash.each do |holiday, supplies|
-      holiday = holiday.to_s.split("_").map {|word| word.capitalize}.join
+      holiday = holiday.to_s.split("_").map {|word| word.capitalize}.join(" ")
       puts "  #{holiday}: #{supplies.join(', ')}"
     end
   end
